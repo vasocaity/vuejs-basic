29d9ca58027deb918ce9c13cc64c0bbcc6e8f69a
waraporn maensiri <wara.phon@odds.team>
2019-02-03 14:41:13 +0700

    add Global BEFORE hooks

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import Router from 'vue-router'
-import routes from './router'
+import { routerConst } from './router'
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(Router);
 
 new Vue({
   render: h => h(App),
-  router: routes,
-}).$mount('#app')
+  router: routerConst,
+}).$mount('#app');
 //yarn add ...
--- src/router.js
+++ src/router.js
@@ -4,47 +4,90 @@ import MyComponent from '@/components/MyComponent.vue'
 import User from '@/components/user/User.vue'
 import UserProfile from '@/components/user/UserProfile.vue'
 import UserSettings from '@/components/user/UserSettings.vue'
-export default new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      component: MyComponent
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/user',
-      component: User,
-      name: 'user',
-      children: [
-        {
-          path: 'post/:post_id',
-          component: User
-        },
-      ]
-    },
-    {
-      path: '/name-view',
-      component: User,
-      children: [
-        {
-          path: 'profile',
-          components: {
-            default: UserProfile,
-            helper: UserSettings
-          } 
+
+const routes = [
+  {
+    path: '/',
+    component: MyComponent
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/user',
+    component: User,
+    name: 'user',
+    // beforeEnter: (to, from, next) => {
+    //   // ...
+    // },
+    children: [
+      {
+        path: 'post/:post_id',
+        component: User
+      },
+    ]
+  },
+  {
+    path: '/name-view',
+    component: User,
+    children: [
+      {
+        path: 'profile',
+        components: {
+          default: UserProfile,
+          helper: UserSettings
         }
-      ]
-    },
-    {
-      //Catch all / 404 Not found Route
-      // will match everything
-      path: '*',
-      component: Login
-    }
-  ]
+      }
+    ]
+  },
+  {
+    //Catch all / 404 Not found Route
+    // will match everything
+    path: '*',
+    component: Login
+  },
+  {
+    path: '/a',
+    component: UserProfile,
+    // alias: '/b'
+  },
+  {
+    path: '/b',
+    component: UserSettings,
+    // alias: '/b'
+  },
+  {
+    path: '/a',
+    redirect: '/b'
+  }
+];
+
+export const routerConst = new Router({
+  mode: 'history',
+  routes // short for routes: routes
+})
+
+// Global BEFORE hooks:
+routerConst.beforeEach((to, from, next) => {
+  // console.log('Global -- beforeEach - fired')
+  
+  // re-route
+  if (to.path === '/a') {
+    next('/')
+  } 
+  // Abort navigation based on some criteria:
+  // else if (to.path === '/user/1') {
+  //   next(false)
+  // } 
+  else if (to.path === '/error') {
+    const err = new Error('My Error Message')
+    
+    // pass the error to onError() callback.
+    next(err)
+  }
+  else {
+    next()
+  }
 })
